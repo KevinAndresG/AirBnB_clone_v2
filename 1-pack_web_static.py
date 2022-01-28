35bf202c4c94e65c7cd748798606356bdf29d48f
KevinAndresG <3348@holbertonschool.com>
2022-01-27 22:19:48 -0500

    fixing

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-	Write a Fabric script that generates a .tgz archive
-	from the contents of the web_static folder
-	of your AirBnB Clone
+    Write a Fabric script that generates a .tgz archive
+    from the contents of the web_static folder
+    of your AirBnB Clone
 """
 
 
@@ -16,7 +16,8 @@ def do_pack():
     try:
         local("mkdir -p versions")
         timenow = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        local("tar -cvzf versions/web_static_{}.tgz web_static".format(timenow))
+        local("tar - cvzf versions/web_static_{}.tgz web_static"
+              .format(timenow))
         return ("versions/web_static_{}.tgz".format(timenow))
     except:
         return (None)
