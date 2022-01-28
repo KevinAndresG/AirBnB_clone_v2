7ae053a094ea11ba822e6d536fc87b30b2992521
KevinAndresG <3348@holbertonschool.com>
2022-01-27 22:06:48 -0500

    files compresed

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+	Write a Fabric script that generates a .tgz archive
+	from the contents of the web_static folder
+	of your AirBnB Clone
+"""
+
+
+def do_pack():
+    """
+            make the .tgz file
+    """
+    from fabric.api import local
+    import datetime
+
+    try:
+        local("mkdir -p versions")
+        timenow = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        local("tar -cvzf versions/web_static_{}.tgz web_static".format(timenow))
+        return ("versions/web_static_{}.tgz".format(timenow))
+    except:
+        return (None)
